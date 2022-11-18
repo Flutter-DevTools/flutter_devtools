24b18146c36d0043a693bd1a8e315028031e98f2
Shadd <7162036-Shadord@users.noreply.gitlab.com>
2022-11-18 17:01:09 +0100

    feat(theme): implement theme Refs: #1

--- lib/presentation/theme/theme_data/app_theme.dart
+++ lib/presentation/theme/theme_data/app_theme.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_devtools/presentation/theme/colors/interfaces/app_colors_interface.dart';
+import 'package:flutter_devtools/presentation/theme/colors/variations/app_colors.dart';
+import 'package:flutter_devtools/presentation/theme/extensions/material_state_property_extension.dart';
+
+part 'button_theme.dart';
+part 'card_theme.dart';
 
 abstract class AppTheme {
   static ThemeData build({
@@ -9,6 +14,11 @@ abstract class AppTheme {
     return ThemeData(
       backgroundColor: appColors.background,
       brightness: brightness,
+
+      // Buttons
+      elevatedButtonTheme: _buildElevatedButtonTheme(appColors: appColors),
+      outlinedButtonTheme: _buildOutlinedButtonTheme(appColors: appColors),
+
     );
   }
 }
---
+++ lib/presentation/theme/theme_data/button_theme.dart
@@ -0,0 +1,33 @@
+part of 'app_theme.dart';
+
+ElevatedButtonThemeData _buildElevatedButtonTheme({
+  required AppColorsInterface appColors,
+}) {
+  return ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: appColors.primary.v100.materialPropertyBuilder(
+        pressed: appColors.primary.v100,
+        disabled: appColors.neutral.v30,
+      ),
+      foregroundColor: pureColors.neutral.v0.mspAll(),
+      animationDuration: kThemeAnimationDuration,
+      overlayColor: appColors.primary.v60.withOpacity(0.2).mspAll(),
+    ),
+  );
+}
+
+OutlinedButtonThemeData _buildOutlinedButtonTheme({
+  required AppColorsInterface appColors,
+}) {
+  return OutlinedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: appColors.primary.v100.materialPropertyBuilder(
+        pressed: appColors.primary.v100,
+        disabled: appColors.neutral.v30,
+      ),
+      foregroundColor: pureColors.neutral.v0.mspAll(),
+      animationDuration: Duration.zero,
+    ),
+  );
+}
+
