b61812b1d66a8be6c5d1e78fc9396b8dac1b23e2
Philippe Dubois <louis.philippe.dubois@outlook.com>
2022-11-18 17:04:31 +0100

    feat(initTheme): fix hydrated bloc on web #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_devtools/presentation/app/app.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
@@ -6,7 +7,9 @@ import 'package:path_provider/path_provider.dart' as path_provider;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: await path_provider.getApplicationDocumentsDirectory(),
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await path_provider.getApplicationDocumentsDirectory(),
   );
   runApp(const App());
 }
