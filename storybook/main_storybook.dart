1f2457b7db8ad9af1bfbb9e342e523a5025b2fe4
Shadd <7162036-Shadord@users.noreply.gitlab.com>
2022-11-23 11:19:13 +0100

    feat(storybook): implement storybook Refs: #15

---
+++ storybook/main_storybook.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+import 'app_storybook.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await path_provider.getApplicationDocumentsDirectory(),
+  );
+  runApp(const AppStorybook());
+}
